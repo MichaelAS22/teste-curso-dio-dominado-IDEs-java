a8ef1585305f33378e4340b5e67889fcfb4d30fd
micha <michael-ale@hotmail.com>
2022-05-01 18:57:04 -0300

    Criação de uma calculadora

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -11,10 +11,6 @@ public class PrimeiroPrograma {
     	
     	System.out.println(cat);
     	System.out.println(livros);
-    	/*
-    	int a = 2;
-    	int b = 3;
-	    System.out.println("Hello World!" +  (a+b));*/
     }
 
 }
